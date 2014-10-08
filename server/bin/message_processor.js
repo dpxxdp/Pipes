457d2bed2343f8aef309b248c6de4dffb205ef3d
dpxxdp <dpxxdp@gmail.com>
2014-10-08 17:15:12 -0400

    first commit, https server

---
+++ server/bin/message_processor.js
@@ -0,0 +1,6 @@
+
+
+
+
+
+function ProcessAdminGET
---
+++ server/bin/server_settings.js
@@ -0,0 +1,5 @@
+var listenngPort = 8000;
+
+var sslKey = 'test/fixtures/keys/agent2-key.pem';
+var sslCert = 'test/fixtures/keys/agent2-cert.pem';
+
