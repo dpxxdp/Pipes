460f76b9e0dff33c06f0e9b9a1714c23df9a5a1a
Daniel Barrett <dan.pk.bar@gmail.com>
2014-10-13 13:46:46 -0400

    kerberos, bson, db_client

--- server/server/biz.js
+++ server/server/biz.js
@@ -1,3 +1,5 @@
+var mongoclient = require('mongodb').MongoClient
+var format = require('util').format;
 
 //bring in cache and mongo
 
@@ -5,41 +7,60 @@ function GetNext(params, callback) {
     
     var low_i = param[0];
     var high_i = param[1];
+
+    mongoclient.connect('mongodb://127.0.0.1:27017/test', function(error, db) {
+      if(error) callback(error)
     
-    //cache query for those rows
-    //else mongo query for those rows
-    //if we get an error
-    //callback(error)
-    //if we get good data
-    //callback(null, customerDataBatch)    
+      var collection = db.collection('test_insert');
+      collection.insert({a:2}, function(err, docs) {
+        
+        collection.count(function(err, count) {
+          console.log(format("count = %s", count));
+        });
     
+        // Locate all the entries using find
+        collection.find().toArray(function(err, results) {
+          console.dir(results);
+          // Let's close the db
+          db.close();
+        });
+      });
+    })
 }
 
 
 function GetAll(params, callback) {
     
-    var options = params;
-    
-    //cache query for those rows
-    //else mongo query for those rows
-    //if we get an error
-    //callback(error)
-    //if we get good data
-    //callback(null, customerDataBatch)    
+    mongoclient.connect('mongodb://127.0.0.1:27017/test', function(error, db) {
+      if(error) callback(error)
+        
+        
+    collection.find().toArray(function(err, results) {
+      console.log("mongodb got all");
+      
+      callback(null, results);
+      db.close();
+    });
     
 }
 
 
 function Push(params, callback) {
     
-    var low_i = param[0];
-    var high_i = param[1];
+    var bizData = params[0];
     
-    //cache query for those rows
-    //else mongo query for those rows
-    //if we get an error
-    //callback(error)
-    //if we get good data
-    //callback(null)    
+    mongoclient.connect('mongodb://127.0.0.1:27017/test', function(error, db) {
+      if(error) callback(error)
     
+      var collection = db.collection(bizData[id]);
+      collection.insert(bizData, function(error, docs) {
+        if (error) callback(error);
+        
+        collection.count(function(error, count) {
+            if (error) callback(error);
+            console.log(format("mongodb: inserted %s row(s)", count));
+            db.close();
+        });
+      });
+    })
 }
