7d134d3f7d27a0905606f5e240bc5edfdfa672ea
dpxxdp <dpxxdp@users.noreply.github.com>
2014-10-15 15:28:58 -0400

    create enum.js

---
+++ common/biz/enum.js
@@ -0,0 +1,8 @@
+
+
+
+exports.P_DB_PROCEDURES = {
+  INSERT : { name : "insert", params : [ "customerData" ] },
+  FIND : { name: "find", params : [ "searchFunction" ] },
+  UPDATE : { name: "update", params : [ "customerData" ] }
+}
