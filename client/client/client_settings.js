23adfc37009f3cb5e1155ce0470d6a36bac66d46
dpxxdp <dpxxdp@users.noreply.github.com>
2014-10-16 09:38:43 -0400

    create client_settings

---
+++ client/client/client_settings.js
@@ -0,0 +1,16 @@
+
+
+exports.requestOptions = {
+    hostname: '127.0.0.1',
+    port: 8000,
+    path: '/',
+    method: 'POST',
+    rejectUnauthorized: false,
+    requestCert: true,
+    agent: false
+}
+
+
+exports.procedure_auth = '12345';
+
+exports.uniqueId = '98765';
