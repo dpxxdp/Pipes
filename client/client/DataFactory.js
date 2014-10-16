6160835cf08a7e15cd9fa942b0fd2d1dba91d89b
dpxxdp <dpxxdp@users.noreply.github.com>
2014-10-16 08:54:55 -0400

    Create DataFactory.js

---
+++ client/client/DataFactory.js
@@ -0,0 +1,32 @@
+
+exports.CreateCustomerData = function (newEmail, newDesc, newWeight, newLocation, newImg) {
+    var customerData = {
+        email : newEmail,
+        desc : newDesc,
+        weight : newWeigt,
+        location : newLocation,
+        img : newImg
+    };
+    return customerData;
+}
+
+exports.CreatePDatabaseRequest = function (newOperation, newParams, newAuth, newId) {
+    var request = {
+        operation : newOperation,
+        params : newParams,
+        auth : newAuth,
+        id : newId
+    };
+    return request;
+}
+
+
+exports.CreateJsonRpcRequest = function (newMethod, newParams, newId) {
+    var request = {
+        jsonrpc : "2.0",
+        method : newMethod,
+        params : newParams,
+        id : newId
+    }
+    return request;
+}
