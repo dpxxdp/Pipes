c1613c4a14a115371dc073a1f3f942983a70e671
dpxxdp <dpxxdp@users.noreply.github.com>
2014-10-16 08:27:59 -0400

    update mongo_client.js

--- server/server/procedure_types/db_clients/mongo_client.js
+++ server/server/procedure_types/db_clients/mongo_client.js
@@ -21,7 +21,7 @@ exports.DoProcedure_CallBackWithResults(operation, params, callback) {
                 var collection = db.collection(collectionName);
                 console.log(format("mongo_client: init " + collectionName));
                 db.close();
-                callback(null, collectionNAme);
+                callback(null, collectionName);
                 
             });
             break;
@@ -79,74 +79,4 @@ exports.DoProcedure_CallBackWithResults(operation, params, callback) {
             return callback("p_database: unrecognized operation request: " + JSON.stringify(operation.name));
             break;
     }
-}
-
-
-
-
-
-
-
-exports.GetByEmail_CallBackWithCustomerData = function (params, callback) {
-    
-    var email = params[0];
-    
-    mongoclient.connect(mongoAddress, function(error, db) {
-        if(error) callback(error)
-        else {
-            
-            db.shippingData.find({'email':email}).toArray(function(error, results) {
-                if (error) callback(error);
-                
-                callback(null, results);
-                db.close();
-            });
-        }
-    });
-}
-
-
-exports.GetAll_CallBackWithCustomerDataBatch = function (params, callback) {
-    
-    mongoclient.connect(mongoAddress, function(error, db) {
-        if(error) callback(error)
-        else {
-            var customerDocument = db.collection();
-            customerDocument.find({}).toArray(function(error, results) {
-            if (error) {callback(error)}
-            
-            console.log("mongodb got all");
-            callback(null, results);
-            db.close();
-            });
-        }
-    });
-}
-
-
-exports.Push_CallBackWithNoRowsInserted = function (params, callback) {
-    console.log("mongo_client: connecting to mongodb");
-    var customerData = params;
-    
-    mongoclient.connect(mongoAddress, function(error, db) {
-        if(error) callback(error);
-        else {
-            var testCollection = db.collection('testCollection');
-            testCollection.insert(customerData, function(error, docs) {
-                console.log("mongo_client: test_collection just called back");
-                if (error) callback(error);
-                else {
-                    testCollection.count(function(error, count) {
-                        if (error) callback(error);
-                        else {
-                            console.log(format("mongo_client: inserted %s row(s)", count));
-                            console.log("mongo_client: inserted: " + customerData)
-                            db.close();
-                            callback(null, count);
-                        }
-                    });
-                }
-            });
-        }
-    });
 }
